a166c07c61eca493aeff10cd33a91a877c30c516
micheledesolda <micheledesolda@yahoo.it>
2024-10-07 17:31:21 +0200

    Improved groves handling

--- global_optimization_velocity_homogeneus.py
+++ global_optimization_velocity_homogeneus.py
@@ -40,49 +40,24 @@ def find_sync_values(mech_data):
 ##########################################################################################################################################
 ## HERE IT IS CLEAR WHY WE NEED A DATABASE FOR THE VARIOUS DDS BLOCKS !!!
 
-# commented the Mauro and De Solda blocks features 
-# General Parameters and Constants
-frequency_cutoff = 2  # [MHz] maximum frequency of the data we want to reproduce
-
-# Constants throughout the entire experiment
-h_groove = 0.1                      # [cm] hight of blocks grooves. Must be allowed to be different for central and sid blocks
-side_block_1 = 2                    # [cm] width of first side block, with grooves
-side_block_2 = 2                    # [cm] width of second side block with grooves
-central_block = 4.8                 # [cm] width of central block, with grooves
-pzt_layer_width = 0.1               # [cm] piezoelectric transducer width
-pmma_layer_width = 0.1              # [cm] pmmate supporting the PZT
-steel_velocity = 3374 * (1e2 / 1e6) # [cm/μs] steel shear wave velocity
-pzt_velocity = 2000 * (1e2 / 1e6)   # [cm/μs] PZT shear wave velocity
-pmma_velocity = 0.4 * 0.1392        # [cm/μs] pmmate supporting the PZT
-
-# Initial guessed velocity model of the sample
-cmin = 800 * (1e2 / 1e6)        
-cmax = 1500 * (1e2 / 1e6) 
-c_step = 10 * (1e2 / 1e6)
-initial_gouge_velocity_list = np.arange(cmin, cmax, c_step)  # Initial velocity range to test
-
-# Fixed travel time through constant sample dimensions
-pzt_depth = 1        # [cm] Position of the PZT with respect to the external side of the block
-transmitter_position = pzt_depth    # [cm] Position of the transmitter from the beginning of the sample
-
-# General Parameters and Constants
-frequency_cutoff = 2  # [MHz] maximum frequency of the data we want to reproduce
-minimum_SNR = 4       # the minimum signal to noise ratio acceted to start computation
+# # commented the Mauro and De Solda blocks features 
+# # General Parameters and Constants
+# frequency_cutoff = 2  # [MHz] maximum frequency of the data we want to reproduce
 
 # # Constants throughout the entire experiment
 # h_groove = 0.1                      # [cm] hight of blocks grooves. Must be allowed to be different for central and sid blocks
-# side_block_1 = 2.93                    # [cm] width of first side block, with grooves
-# side_block_2 = 2.93                    # [cm] width of second side block with grooves
-# central_block = 4.88                 # [cm] width of central block, with grooves
+# side_block_1 = 2                    # [cm] width of first side block, with grooves
+# side_block_2 = 2                    # [cm] width of second side block with grooves
+# central_block = 4.8                 # [cm] width of central block, with grooves
 # pzt_layer_width = 0.1               # [cm] piezoelectric transducer width
-# pmma_layer_width = 0.              # [cm] pmma supporting the PZT: in this blocks there are not
+# pmma_layer_width = 0.1              # [cm] pmmate supporting the PZT
 # steel_velocity = 3374 * (1e2 / 1e6) # [cm/μs] steel shear wave velocity
 # pzt_velocity = 2000 * (1e2 / 1e6)   # [cm/μs] PZT shear wave velocity
 # pmma_velocity = 0.4 * 0.1392        # [cm/μs] pmmate supporting the PZT
 
-# # Initial guessed velocity model of the sample: literature range for gouge at atmospheric preassure
-# cmin = 200 * (1e2 / 1e6)        
-# cmax = 600 * (1e2 / 1e6) 
+# # Initial guessed velocity model of the sample
+# cmin = 800 * (1e2 / 1e6)        
+# cmax = 1500 * (1e2 / 1e6) 
 # c_step = 10 * (1e2 / 1e6)
 # initial_gouge_velocity_list = np.arange(cmin, cmax, c_step)  # Initial velocity range to test
 
@@ -90,14 +65,41 @@ minimum_SNR = 4       # the minimum signal to noise ratio acceted to start compu
 # pzt_depth = 1        # [cm] Position of the PZT with respect to the external side of the block
 # transmitter_position = pzt_depth    # [cm] Position of the transmitter from the beginning of the sample
 
-fixed_travel_time = (2 * (side_block_1 - transmitter_position) + central_block - 4*h_groove) / steel_velocity  # travel time of direct wave into the blocks
+# General Parameters and Constants
+frequency_cutoff = 2  # [MHz] maximum frequency of the data we want to reproduce
+minimum_SNR = 4       # the minimum signal to noise ratio acceted to start computation
+
+# Constants throughout the entire experiment
+side_block_1 = 2.93                 # [cm] width of first side block, with grooves
+side_block_2 = 2.93                 # [cm] width of second side block with grooves
+h_groove_side = 0.059                # [cm] hight of blocks grooves
+central_block = 4.88                # [cm] width of central block, with grooves
+h_groove_central = 0.096             # [cm] hight of blocks grooves
+pzt_layer_width = 0.1               # [cm] piezoelectric transducer width
+pmma_layer_width = 0.               # [cm] pmma supporting the PZT: in this blocks there are not
+steel_velocity = 3374 * (1e2 / 1e6) # [cm/μs] steel shear wave velocity
+pzt_velocity = 2000 * (1e2 / 1e6)   # [cm/μs] PZT shear wave velocity
+pmma_velocity = 0.4 * 0.1392        # [cm/μs] pmmate supporting the PZT
+
+# Initial guessed velocity model of the sample: literature range for gouge at atmospheric preassure
+cmin = 300 * (1e2 / 1e6)        
+cmax = 600 * (1e2 / 1e6) 
+c_step = 10 * (1e2 / 1e6)
+initial_gouge_velocity_list = np.arange(cmin, cmax, c_step)  # Initial velocity range to test
+
+# Fixed travel time through constant sample dimensions
+pzt2grove = 1.71        # [cm] distance between the PZT and the top of the groves
+pzt_depth = side_block_1-pzt2grove        # [cm] Position of the PZT with respect to the external side of the block
+transmitter_position = pzt_depth    # [cm] Position of the transmitter from the beginning of the sample
+
+fixed_travel_time = (2 * (side_block_1 - transmitter_position) + central_block - 2*h_groove_side - 2*h_groove_central) / steel_velocity  # travel time of direct wave into the blocks
 
 ## GET OBSERVED DATA
 
 # Load and process the pulse waveform: it is going to be our time source function
 machine_name_pulse = "on_bench"
 experiment_name_pulse = "glued_pzt"
-data_type_pulse = "data_analysis/wavelets_from_glued_pzt"
+data_type_pulse = "data_analysis/wavelets_from_PIS1_PIS2_glued_250ns"
 infile_path_list_pulse = make_infile_path_list(machine_name=machine_name_pulse, experiment_name=experiment_name_pulse, data_type=data_type_pulse)
 
 # Assuming only one pulse is used, we can load it outside the loop
@@ -106,6 +108,8 @@ pulse_waveform, pulse_metadata = load_waveform_json(pulse_path)
 pulse_time = pulse_metadata['time_ax_waveform']
 pulse_waveform, _ = signal2noise_separation_lowpass(pulse_waveform, pulse_metadata, freq_cut=frequency_cutoff)
 pulse_waveform = pulse_waveform - pulse_waveform[0]  # Make the pulse start from zero
+# plt.plot(pulse_time,pulse_waveform)
+# plt.show()
 
 dt_pulse = pulse_time[1] - pulse_time[0]
 pulse_duration = pulse_time[-1] - pulse_time[0]
@@ -114,15 +118,15 @@ pulse_duration = pulse_time[-1] - pulse_time[0]
 
 # DATA FOLDERS
 machine_name = "Brava_2"
-experiment_name = "s0108"
-data_type_uw = 'data_tsv_files'
+experiment_name = "s0176"
+data_type_uw = 'data_tsv_files_sv1_sv2_only'
 data_type_mech = 'mechanical_data'
 sync_file_pattern = '*s*_data_rp'  # Pattern to find specific experiment in mechanical data
 
 # LOAD MECHANICAL DATA (Only once)
 infile_path_list_mech = make_infile_path_list(machine_name, experiment_name, data_type=data_type_mech)
 mech_data_path = find_mechanical_data(infile_path_list_mech, sync_file_pattern)
-mech_data = pd.read_csv(mech_data_path, sep=',', skiprows=[1])
+mech_data = pd.read_csv(mech_data_path, sep='\t', skiprows=[1])
 sync_data, sync_peaks = find_sync_values(mech_data)
 
 if sync_peaks is None:
@@ -146,8 +150,8 @@ estimated_velocities = []     # To store the estimated velocity for each wavefor
 
 # Main Loop Over UW Files
 for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
-    # if chosen_uw_file == 0:
-    #     continue
+    if chosen_uw_file == 0:
+        continue
 
     # CHOOSE OUTFILE_PATH
     outfile_name = os.path.basename(infile_path).split('.')[0]
@@ -182,19 +186,20 @@ for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
         thickness_gouge_1_list = mech_data.rgt_lt_mm[sync_peaks[2 * chosen_uw_file]: sync_peaks[2 * chosen_uw_file + 1]].values / 10  # Convert mm to cm
         thickness_gouge_2_list = thickness_gouge_1_list  # Assuming both layers have the same thickness
         normal_stress_list = mech_data.normal_stress_MPa[sync_peaks[2 * chosen_uw_file]: sync_peaks[2 * chosen_uw_file + 1]].values 
+        shear_stress_list = mech_data.shear_stress_MPa[sync_peaks[2 * chosen_uw_file]: sync_peaks[2 * chosen_uw_file + 1]].values 
 
     except TypeError:
         thickness_gouge_1_list = mech_data.rgt_lt_mm[sync_peaks[2 * chosen_uw_file]: metadata['number_of_waveforms']].values / 10  # Convert mm to cm
         thickness_gouge_2_list = thickness_gouge_1_list  # Assuming both layers have the same thickness
         normal_stress_list = mech_data.normal_stress_MPa[sync_peaks[2 * chosen_uw_file]: metadata['number_of_waveforms']].values
+        shear_stress_list = mech_data.shear_stress_MPa[sync_peaks[2 * chosen_uw_file]: metadata['number_of_waveforms']].values
         
     # Initialize previous_min_velocity for the first waveform
     previous_min_velocity = None
 
     # Process each waveform sequentially
-    for idx_waveform, (thickness_gouge_1, thickness_gouge_2, normal_stress) in enumerate(zip(thickness_gouge_1_list[::downsampling], thickness_gouge_2_list[::downsampling], normal_stress_list[::downsampling])):
-        print(f"Layer thickness: {thickness_gouge_1}")
-        print(f"Normal_stress: {normal_stress}")
+    for idx_waveform, (thickness_gouge_1, thickness_gouge_2, normal_stress, shear_stress) in enumerate(zip(thickness_gouge_1_list[::downsampling], thickness_gouge_2_list[::downsampling], normal_stress_list[::downsampling], shear_stress_list[::downsampling])):
+        print(f"Layer thickness: {thickness_gouge_1}\tNormal_stress: {normal_stress}\tshear_stress: {shear_stress}")
         idx = idx_waveform * downsampling
         observed_waveform = observed_waveform_data[idx] 
 
@@ -205,26 +210,26 @@ for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
             max_signal = np.amax(observed_waveform[good_data_interval])       
             max_noise = np.amax(observed_waveform[sure_noise_interval])
             if max_signal/max_noise < minimum_SNR:
-                print(f"Signal to Noise ratio for waveform {idx_waveform} = {max_signal/max_noise}. Skipped computation")
+                print(f"Signal to Noise ratio for waveform {idx} = {max_signal/max_noise}. Skipped computation")
                 continue
 
             # First waveform, use initial velocity range, corrected for an empirical depdendence from normal stress
 
-            correction_normal_stress = (1/normal_stress**0.25)*normal_stress**0.25
+            correction_normal_stress = normal_stress**0.25
             cmin_waveform = cmin * correction_normal_stress
             cmax_waveform = cmax * correction_normal_stress
             c_step_waveform = c_step
             print(f"Evaluating shear wave velocity in the interval: {cmin_waveform:4f}-{cmax_waveform:4f}")
         else:
             # For subsequent waveforms, center around previous min velocity
-            c_range = 50 * (1e2 / 1e6)  # [cm/μs], adjust as needed
+            c_range = 200 * (1e2 / 1e6)  # [cm/μs], adjust as needed
             cmin_waveform = previous_min_velocity - c_range
             cmax_waveform = previous_min_velocity + c_range
-            c_step_waveform = c_step / 2  # Use a smaller step size for better resolution
+            c_step_waveform = c_step   # Use a smaller step size for better resolution
 
         # DEFINE EVALUATION INTERVAL FOR L2 NORM OF THE RESIDUALS
-        max_travel_time = fixed_travel_time + thickness_gouge_1 / cmin_waveform + thickness_gouge_2 / cmin_waveform 
-        min_travel_time = fixed_travel_time + thickness_gouge_1 / cmax_waveform + thickness_gouge_2 / cmax_waveform
+        max_travel_time = fixed_travel_time + thickness_gouge_1 / cmin_waveform + thickness_gouge_2 / cmin_waveform + 0.5*(2*h_groove_side + 2*h_groove_central )/ (steel_velocity + cmin_waveform)
+        min_travel_time = fixed_travel_time + thickness_gouge_1 / cmax_waveform + thickness_gouge_2 / cmax_waveform + 0.5*(2*h_groove_side + 2*h_groove_central )/ (steel_velocity + cmax_waveform)
         misfit_interval = np.where((observed_time > min_travel_time) & (observed_time < max_travel_time + pulse_duration))
 
         # Create velocity list for this waveform
@@ -270,7 +275,8 @@ for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
                 pulse_time=pulse_time,
                 pulse_waveform=pulse_waveform,
                 sample_dimensions=sample_dimensions,
-                h_groove=h_groove,
+                h_groove_side=h_groove_side,
+                h_groove_central = h_groove_central,
                 frequency_cutoff=frequency_cutoff,
                 transmitter_position=transmitter_position,
                 receiver_position=receiver_position,
@@ -280,6 +286,7 @@ for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
                 gouge_velocity=gouge_velocity,
                 pzt_velocity=pzt_velocity,
                 pmma_velocity=pmma_velocity,
+                misfit_interval = misfit_interval,
                 normalize_waveform=True,
                 enable_plotting=False)
 
@@ -306,37 +313,39 @@ for chosen_uw_file, infile_path in enumerate(infile_path_list_uw):
         min_idx = np.argmin(L2norm_waveform)
         previous_min_velocity = gouge_velocity_list_waveform[min_idx]
         estimated_velocities.append(previous_min_velocity)
-        print(f"Waveform {idx_waveform}: Minimum misfit at gouge_velocity = {previous_min_velocity:.4f} cm/μs")
+        print(f"Waveform {idx}: Minimum misfit at gouge_velocity = {previous_min_velocity:.4f} cm/μs")
 
-        # After finding the best-fit velocity, generate and plot the synthetic waveform
-        best_gouge_velocity = previous_min_velocity
+        # # After finding the best-fit velocity, generate and plot the synthetic waveform
+        # best_gouge_velocity = previous_min_velocity
 
-        # Use the same sample_dimensions and positions as before
-        sample_dimensions = [side_block_1, thickness_gouge_1, central_block, thickness_gouge_2, side_block_2]
-        receiver_position = pzt_depth  # [cm] Receiver is in the side_block_2
+        # # Use the same sample_dimensions and positions as before
+        # sample_dimensions = [side_block_1, thickness_gouge_1, central_block, thickness_gouge_2, side_block_2]
+        # receiver_position = pzt_depth  # [cm] Receiver is in the side_block_2
 
-        # Call DDS_UW_simulation with enable_plotting=True
-        movie_output_path = f"waveform_{idx_waveform}_velocity_{best_gouge_velocity:4f}.mp4"
-        synthetic_waveform = DDS_UW_simulation(
-            observed_time=observed_time,
-            observed_waveform=observed_waveform,
-            pulse_time=pulse_time,
-            pulse_waveform=pulse_waveform,
-            sample_dimensions=sample_dimensions,
-            h_groove=h_groove,
-            frequency_cutoff=frequency_cutoff,
-            transmitter_position=transmitter_position,
-            receiver_position=receiver_position,
-            pzt_layer_width=pzt_layer_width,
-            pmma_layer_width=pmma_layer_width,
-            steel_velocity=steel_velocity,
-            gouge_velocity=best_gouge_velocity,
-            pzt_velocity=pzt_velocity,
-            pmma_velocity=pmma_velocity,
-            normalize_waveform=True,
-            enable_plotting=True,
-            make_movie=False,
-            movie_output_path=movie_output_path)
+        # # Call DDS_UW_simulation with enable_plotting=True
+        # movie_output_path = f"waveform_{idx_waveform}_velocity_{best_gouge_velocity:4f}.mp4"
+        # synthetic_waveform = DDS_UW_simulation(
+        #     observed_time=observed_time,
+        #     observed_waveform=observed_waveform,
+        #     pulse_time=pulse_time,
+        #     pulse_waveform=pulse_waveform,
+        #     sample_dimensions=sample_dimensions,
+        #     h_groove_side=h_groove_side,
+        #     h_groove_central = h_groove_central,            
+        #     frequency_cutoff=frequency_cutoff,
+        #     transmitter_position=transmitter_position,
+        #     receiver_position=receiver_position,
+        #     pzt_layer_width=pzt_layer_width,
+        #     pmma_layer_width=pmma_layer_width,
+        #     steel_velocity=steel_velocity,
+        #     gouge_velocity=best_gouge_velocity,
+        #     pzt_velocity=pzt_velocity,
+        #     pmma_velocity=pmma_velocity,
+        #     misfit_interval=misfit_interval,
+        #     normalize_waveform=True,
+        #     enable_plotting=True,
+        #     make_movie=True,
+        #     movie_output_path=movie_output_path)
 
 
     # After processing all waveforms in the current UW file, save the results
