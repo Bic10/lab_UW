Fixed signs in gradient formulas

lab_uw/data_io.py
@@ -258,12 +258,7 @@ class UltrasonicDataHandler:
 
         # Lowpass filtering
         if frequency_cutoff:
-            signal_processor = SignalProcessor()
-            observed_waveform_data, _ = signal_processor.signal2noise_separation_lowpass(
-                waveform_data=observed_waveform_data,
-                metadata=metadata,
-                freq_cut=frequency_cutoff
-            )
+            observed_waveform_data = butter_bandpass_filter(observed_waveform_data, 0.25, frequency_cutoff, 1/metadata["sampling_rate"])
 
         if time_ax_acquisition_start:
             metadata["time_ax_acquisition"] = metadata["time_ax_acquisition"] + time_ax_acquisition_start
@@ -314,12 +309,9 @@ class UltrasonicDataHandler:
             np.array(stf_metadata["time_ax_waveform"]) 
             - np.array(stf_metadata["time_ax_waveform"])[0]
         )
-        signal_processor = SignalProcessor()
-        stf_waveform_filt, _ = signal_processor.signal2noise_separation_lowpass(
-            waveform_data=stf_waveform_raw,
-            metadata=stf_metadata,
-            freq_cut=frequency_cutoff
-        )
+
+        stf_waveform_filt = butter_bandpass_filter(stf_waveform_raw, 0.25, frequency_cutoff, 1/stf_metadata["sampling_rate"])
+
         stf_waveform = stf_waveform_filt - stf_waveform_filt[0]
 
         stf_handler.waveform_data = stf_waveform
@@ -656,3 +648,12 @@ class BlockMetadataHandler:
             results.append(block_handler.get_block_params(bk))
 
         return tuple(results)
+    
+from scipy.signal import butter, lfilter
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    return butter(order, [lowcut, highcut], fs=fs, btype='band')
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y

lab_uw/forward_modeling.py
@@ -534,7 +534,7 @@ class UltrasonicModeler:
                     # -------------------------------------------------------------------
                     gradient_vel = np.zeros_like(best_velocity_model)
                     # temp = wavefield_adjoint
-                    temp = (2.0 * best_velocity_model[None, :]) * np.flipud(wavefield_adjoint * best_laplacian_wavefield)
+                    temp = -(2.0 * best_velocity_model[None, :]) * np.flipud(wavefield_adjoint) * (best_laplacian_wavefield)
                     gradient_vel = np.sum(temp, axis=0) 
                     max_vel_grad = np.max(np.abs(gradient_vel[regions_to_update]))
 
@@ -544,7 +544,7 @@ class UltrasonicModeler:
                 if da_max:
                     gradient_damp = np.zeros_like(best_damping_model)
                     # temp = wavefield_adjoint
-                    temp =  np.flipud(wavefield_adjoint * best_first_derivative_laplacian)
+                    temp =  -np.flipud(wavefield_adjoint) * best_first_derivative_laplacian
                     gradient_damp = np.sum(temp, axis=0) 
                     max_damp_grad = np.max(np.abs(gradient_damp[regions_to_update]))
 
@@ -602,11 +602,11 @@ class UltrasonicModeler:
                 step_size_w   = dw_max / (max_w_grad + 1e-15)
                 updated_source_time_function -= step_size_w * gradient_w
 
-                updated_source_time_function, _ = signal_processor.signal2noise_separation_lowpass(
-                        waveform_data=updated_source_time_function,
-                        metadata=self.stf_handler.metadata,
-                        freq_cut=self.frequency_cutoff
-                    )
+                # updated_source_time_function, _ = signal_processor.signal2noise_separation_lowpass(
+                #         waveform_data=updated_source_time_function,
+                #         metadata=self.stf_handler.metadata,
+                #         freq_cut=self.frequency_cutoff
+                #     )
 
             if ds_max:
                 # -- update spatial distribution s(x) --
@@ -692,14 +692,14 @@ class UltrasonicModeler:
                 break
             
             else:
-                previous_misfit = updated_misfit
-
                 updating = False
                 dc_max /= reduce_factor
                 da_max /= reduce_factor
                 dw_max /= reduce_factor
                 ds_max /= reduce_factor
                 print(f"    ✗ No improvement. Reverting & reducing step")
+
+            previous_misfit = updated_misfit
 
         self.synthetic_waveform                    = best_synthetic_waveform
 

local_optimization_FWI_velocity_1D.py
@@ -215,7 +215,7 @@ def process_uw_file(
             velo_mean =  simulation.average_gouge_velocity
             velo_min  = velo_mean - 0.03 * velo_mean
             velo_max  = velo_mean + 0.03 * velo_mean
-            velo_step = 0.001 * velo_mean
+            velo_step = 0.01 * velo_mean
             params["velocity_initial_list"] = np.arange(velo_min, velo_max, velo_step)
 
             damp_mean =  simulation.average_gouge_damping
@@ -440,7 +440,7 @@ def global_search_waveform(
 
     # Construct output paths
     if save_plot:
-        plot_output_name = f"{outfile_name}_rec_n_{idx_processed_waveform}"
+        plot_output_name = f"{outfile_name}"
         plot_output_path = outdir_path_image / plot_output_name
     else:
         plot_output_path = None
@@ -600,8 +600,8 @@ if __name__ == "__main__":
     data_type_uw    = "uw_data/data_tsv_files" # + wave_type
     data_type_mech  = "mechanical_data"
     mech_file_name  = f"{experiment_name}_data_rp"
-    outfolder_name  = "local_inversion" + wave_type + "_2025_04_14_stf_local_inversion_freesurface_1_velramp_damramp"
-    outfolder_name = "2025_04_14_test"
+    outfolder_name  = "2025_04_14_stf_local_inversion_freesurface_1_velgouge_damgouge_20"
+    # outfolder_name = "2025_04_14_test"
     # Create output directories
     outdir_path_l2norm = dir_manager.make_data_analysis_folders(
         machine_name    = machine_name,
@@ -619,19 +619,19 @@ if __name__ == "__main__":
     params = {
         "absorbing"                 : False,        # set absorbing bounday, merdoso silicone maledetto
         "num_waveform2process"      : None,         # int, equespatially waveforms to sample for processing. Of "None", process all
-        "maxtime2simulate"          : 50,           # [mus]
+        "maxtime2simulate"          : 40,           # [mus]
         "frequency_cutoff"          : 4,            # [MHz] low pass onserved data and simulate up to this frequency
         "minimum_SNR"               : 3,            # skip computation until time interval where signal should be is above SNR times surely-only-noise part 
-        "velocity_initial_list"     : np.linspace(0.1550,0.1750, 5),  # [cm/mus] first guess of best velocity. There is a visual tool for it, if needed
+        "velocity_initial_list"     : np.linspace(0.22,.260, 30),  # [cm/mus] first guess of best velocity. There is a visual tool for it, if needed
         "min_velocity2simulate"     : None,         # [cm/mus] if not passed, computed by assembly and gouge velocity range
         "max_velocity2simulate"     : None,         # [cm/mus]
-        "damping_initial_list"      : np.linspace(0.0001,0.0008, 5),
+        "damping_initial_list"      : np.linspace(0.0007,0.0017, 10),
         "plot_save_interval"        : 1,
         "movie_save_interval"       : 1,
         "l2norm_plot_interval"      : 1,
         "outdir_path_l2norm"        : outdir_path_l2norm[0],
         "outdir_path_image"         : outdir_path_image[0],
-        "n_iterations"              : 40,
+        "n_iterations"              : 70,
         "reduce_factor"             : 10/9
     }
 
@@ -643,7 +643,7 @@ if __name__ == "__main__":
         data_type_stf       = "data_analysis/source_time_functions" + wave_type,
         stf_chosen          = "width250_volt70_multiplier_local_inversion",
         # stf_chosen          = "width250_volt70",
-        frequency_cutoff= 12.5 # params["frequency_cutoff"]
+        frequency_cutoff= 6 # params["frequency_cutoff"]
     )
 
     # stf_handler.waveform_data = -stf_handler.waveform_data
@@ -696,7 +696,7 @@ if __name__ == "__main__":
         uw_data_handler = UltrasonicDataHandler.load_and_process_uw(
             infile_path                 = infile_path,
             zero_out_time               = params["steel_only_time_p_wave"]/5,
-            frequency_cutoff            = params["frequency_cutoff"],
+            frequency_cutoff            = 12.49, # params["frequency_cutoff"],
             maxtime2simulate            = params["maxtime2simulate"],
             number_of_waveforms2process = params["num_waveform2process"],
             time_ax_acquisition_start   = mech_data_slice["time_s"].values[0]

source_receiver_simulation_parameters.py
@@ -44,20 +44,19 @@ def process_uw_file(
     uw_data_handler = UltrasonicDataHandler.load_and_process_uw(
         infile_path=infile_path,
         frequency_cutoff=params["frequency_cutoff"],
-        maxtime2simulate=params["maxtime2simulate"],
+        maxtime2simulate=params["maxtime2simulate"],    
         number_of_waveforms2process=params["number_of_waveforms2process"]
     )
 
     observed_waveform_data = uw_data_handler.waveform_data
     metadata = uw_data_handler.metadata
     observed_time = metadata["time_ax_waveform"]
-    # observed_waveform_data = butter_bandpass_filter(observed_waveform_data, 0.25, params["frequency_cutoff"], metadata["sampling_rate"])
 
     # We only want 1 "mean" waveform for analysis
     observed_waveform = np.mean(observed_waveform_data, axis=0)
 
     # 2) We'll run the Monte Carlo approach multiple times
-    n_repeats = 10
+    n_repeats = 20
 
     # We will store the best parameters from each run in a list of dicts
     # all_best_params = []
@@ -332,7 +331,7 @@ def process_waveform(
         stf_from_inverison_outfile_path = stf_handler.infile.parent / stf_from_inverison_outfile_name
 
 
-        stf_handler.waveform_data = butter_bandpass_filter(stf_handler.waveform_data, 0.25, 6., 25)    
+        stf_handler.waveform_data = butter_bandpass_filter(stf_handler.waveform_data, 0.25, 12.49, stf_handler.metadata["sampling_rate"])    
 
         stf_handler.save_waveform_json(data = stf_handler.waveform_data, 
                                         metadata = stf_handler.metadata, 
@@ -444,7 +443,7 @@ if __name__ == "__main__":
     experiment_name = "STF_ss10_05"
     wave_type       = "_s"  # e.g., compressional wave
     data_type_uw    = f"uw_data/data_tsv_files{wave_type}"
-    outfolder_name  = f"simulation_parameters{wave_type}_2025-04-14_only_STF_30s_stf_bandpass_from_original_multiplier" 
+    outfolder_name  = f"2025-04-14_only_STF_60s_stf_bandpass_from_original_multiplier" 
 
     # Create output directories
     outdir_path_l2norm = dir_manager.make_data_analysis_folders(
@@ -463,7 +462,7 @@ if __name__ == "__main__":
         "absorbing"                 : False,
         "save_local_inversion_STF"  : True,
         "saved_STF_file_name"       : "_local_inversion",   # this string will be added to the "stf_chosen" file name, so to not overdrive the original data
-        "maxtime2simulate"          : 30,   # mus
+        "maxtime2simulate"          : 60,   # mus
         "frequency_cutoff"          : 6,     # MHz
         "minimum_SNR"               : 5,
         "min_velocity2simulate"     : 0.2,  # cm/mus
@@ -493,7 +492,7 @@ if __name__ == "__main__":
 
     #### MONTE CARLO PARAMETERS DEFINED HERE ####
     global_search_space = {
-        "num_iterations"       : 1000,  # how many random draws to try
+        "num_iterations"       : 4000,  # how many random draws to try
         "steel_velocity_low"   : assembly_dict["velocity" + wave_type] - 0.005,
         "steel_velocity_high"  : assembly_dict["velocity" + wave_type] + 0.005,              
         "pzt_velocity_low"     : params["min_velocity2simulate"], # assembly_dict["pzt_velocity" + wave_type], 
@@ -501,8 +500,8 @@ if __name__ == "__main__":
         "spreading_factor_low" : 0.1,
         "spreading_factor_high": 1.0,
         # Uniform range for positions relative to edges pzt-steel
-        "position2edge_low"    : -0.8,
-        "position2edge_high"   : 0.,
+        "position2edge_low"    : -0.5,
+        "position2edge_high"   : -0.5,
         # how many nodes to use to approximate the tx/rx positions in case they do not correspond precisely to one node
         "radius_factor_low"    : 1.0,
         "radius_factor_high"   : 1.0,